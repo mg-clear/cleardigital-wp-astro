1e971e9fbb5bbf09924821e31c151caa3e215efe
mg <micah@cleardigital.com>
2023-09-11 01:49:59 -0400

    organize sets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/ui-kit': '/ui',
+    '/set-1/ui': '/ui'
+  },
   output: "server",
   adapter: netlify()
 });
