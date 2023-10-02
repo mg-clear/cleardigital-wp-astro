a8f0b650a590b24aed6f013208d758a45f6f18d9
Mani Sohal <mani@webenertia.com>
2023-10-02 18:00:06 +0530

    fix project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  output: "server",
   redirects: {
     '/': '/set-1/homepage',
     '/ui-kit': '/ui',
     '/set-1/ui': '/ui'
-  },
-  output: "server",
-  adapter: netlify()
+  }
 });
